441c0f84511b60cfdc647325d6807b3a48ff525d
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2023-08-22 16:46:38 -0400

    added clear button

--- v1.3/main.js
+++ v1.3/main.js
@@ -113,18 +113,17 @@ class Timer {
 
         20min
 
-        </button> 
-      
-      
+        </button>
+
         <button type="button" class="timer__btn timer__btn--reset">😔</button>
       </div>
-      
+
       <div class="timer--top">
         <span class="timer__part timer__part--minutes">00</span>
         <span class="timer__part">/</span>
         <span class="timer__part timer__part--seconds">00</span>
-      
-        <button type="button" class="timer__btn timer__btn--control timer__btn--start">  
+
+        <button type="button" class="timer__btn timer__btn--control timer__btn--start">
           <span class="material-icons">play_arrow</span>
         </button>
       </div>
@@ -133,3 +132,10 @@ class Timer {
 }
 
 new Timer(document.querySelector('.timer'));
+
+function eraseText() {
+    console.log('hello');
+    document.getElementById('textarea').value = '';
+}
+
+function hideTimer() {}
